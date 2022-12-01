411fdc87679147c6ac15760ddeee052fcf3814f3
MMoe22 <morten.1949@hotmail.com>
2022-12-01 18:02:49 +0100

    Add file

---
+++ GitHub.py
@@ -0,0 +1,27 @@
+
+import random
+
+class random_number:
+    
+    def generate(amount: int, minRange: int, maxRange: int):
+        ans = []
+        
+        if amount < 0:
+            return "Amount must be over 0"
+        elif minRange > maxRange:
+            return "minRange must be smaller than maxRange"
+        
+        for i in range(amount):
+            ans.append(random.randint(minRange, maxRange))
+
+        for i in ans:
+            if type(i) != int:
+                return
+
+        if len(ans) < 1:
+            return
+
+        return ans
+
+
+print(random_number.generate(6, 1, 100))
